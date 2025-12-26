f06387c56500a4c914d5d4ff2b8c06c4c1594a4d
juanmaaidar <juanmanuelaidar@gmail.com>
2025-12-26 15:34:23 -0300

    Soluciona errores de compilación tras cambio de arquitectura y limpieza de duplicados

--- mobile/composeApp/src/androidMain/kotlin/org/example/project/network/ApiConfig.android.kt
+++ mobile/composeApp/src/androidMain/kotlin/org/example/project/network/ApiConfig.android.kt
@@ -1,5 +1,4 @@
 package org.example.project.network
 
-// Use 10.0.2.2 to access host localhost from Android Emulator
-actual val SERVER_URL: String = "http://10.0.2.2:9090"
+actual val SERVER_URL: String = "http://10.0.2.2:8080"
 actual val PROXY_URL: String = "http://10.0.2.2:8081"
